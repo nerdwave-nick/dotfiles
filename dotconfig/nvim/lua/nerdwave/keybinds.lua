823c51b68dd775b8b8770d6e556e2739818331bf
nerdwave <nick@nerdwave.io>
2024-02-07 23:09:59 +0100

    feat: font customization and keymap adjustment

--- dotconfig/nvim/lua/nerdwave/keybinds.lua
+++ dotconfig/nvim/lua/nerdwave/keybinds.lua
@@ -1,80 +1,21 @@
 -- SETUP
 local format_with_notif = function()
-	local callback = function(err, did_edit)
-		if err ~= nil then
-			vim.notify(err, vim.log.levels.ERROR, { title = "Formatting failed" })
-			return
-		end
-		if did_edit then
-			vim.notify("Formatted", vim.log.levels.INFO, { title = "Formatting succeeded" })
-		else
-			vim.notify("No changes occurred", vim.log.levels.INFO, { title = "Formatting succeeded" })
-		end
-	end
-	require("conform").format({ lsp_fallback = true }, callback)
+    local callback = function(err, did_edit)
+        if err ~= nil then
+            vim.notify(err, vim.log.levels.ERROR, { title = "Formatting failed" })
+            return
+        end
+        if did_edit then
+            vim.notify("Formatted", vim.log.levels.INFO, { title = "Formatting succeeded" })
+        else
+            vim.notify("No changes occurred", vim.log.levels.INFO, { title = "Formatting succeeded" })
+        end
+    end
+    require("conform").format({ lsp_fallback = true }, callback)
 end
 
 -- ###########################################################################################
 -- KEYMAP ####################################################################################
-
-----------
--- Plugins
-
-local telescope = require("telescope")
-local harpoon = require("harpoon")
-local telescopeBuiltin = require("telescope.builtin")
-
--- vstask
-vim.keymap.set("n", "<leader>vt", function()
-	telescope.extensions.vstask.task()
-end)
--- undotree
-vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
--- harpoon
-vim.keymap.set("n", "<leader>a", function()
-	harpoon:list():append()
-end)
-vim.keymap.set("n", "<C-e>", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
-vim.keymap.set("n", "<C-h>", function()
-	harpoon:list():select(1)
-end)
-vim.keymap.set("n", "<C-t>", function()
-	harpoon:list():select(2)
-end)
-vim.keymap.set("n", "<C-n>", function()
-	harpoon:list():select(3)
-end)
-vim.keymap.set("n", "<C-s>", function()
-	harpoon:list():select(4)
-end)
--- lazygit
-vim.keymap.set("n", "<leader>gg", function()
-	telescope.extensions.lazygit.lazygit()
-end)
--- nvim-comment
-vim.keymap.set("n", "<leader>/", vim.cmd.CommentToggle)
-vim.keymap.set("v", "<leader>/", vim.cmd.CommentToggle)
--- zen-mode
-vim.keymap.set("n", "<leader>zm", function()
-	require("zen-mode").toggle()
-end)
--- gitsigns
-vim.keymap.set("n", "<leader>tb", function()
-	require("gitsigns").toggle_current_line_blame()
-end)
--- telescope
-vim.keymap.set("n", "<leader>pf", telescopeBuiltin.find_files)
-vim.keymap.set("n", "<leader>pp", function()
-	telescopeBuiltin.find_files({ hidden = true })
-end)
-vim.keymap.set("n", "<C-p>", telescopeBuiltin.git_files)
--- vim.keymap.set("n",'<leader>ps', function() telescopeBuiltin.grep_string({search = vim.fn.input("Grep >")}) end)
-vim.keymap.set("n", "<leader>ps", telescopeBuiltin.live_grep)
-vim.keymap.set("n", "<leader>ph", telescopeBuiltin.help_tags)
-vim.keymap.set("n", "<leader>pe", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-vim.keymap.set("n", "<leader>pn", ":Telescope noice<CR>")
 
 ----------
 -- General
@@ -85,7 +26,7 @@ vim.keymap.set("n", "<M-Down>", ":m+1<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<M-k>", ":m-2<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<M-j>", ":m+1<CR>", { silent = true, noremap = true })
 -- move selection up/down
-vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>vgv=vgv", { silent = true, noremap = true })
+vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>vgv=vgv", { silent = true, noremap = false })
 vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>vgv=vgv", { silent = true, noremap = true })
 vim.keymap.set("v", "<M-k>", ":m '<-2<CR>vgv=vgv", { silent = true, noremap = true })
 vim.keymap.set("n", "<M-j>", ":m '>+1<CR>vgv=vgv", { silent = true, noremap = true })
@@ -110,3 +51,74 @@ vim.keymap.set("n", "<C-M-8>", "<C-w>|", { noremap = true })
 vim.keymap.set("n", "<C-M-7>", "<C-w>=", { noremap = true })
 -- terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+-- yoinks
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+-- the name is the 'these are pretty neat'-agen
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+-- take care of te devil
+vim.keymap.set("n", "Q", "<nop>")
+
+----------
+-- Plugins
+
+local telescope = require("telescope")
+local harpoon = require("harpoon")
+local telescopeBuiltin = require("telescope.builtin")
+
+-- vstask
+vim.keymap.set("n", "<leader>vt", function()
+    telescope.extensions.vstask.task()
+end)
+-- undotree
+vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+-- harpoon
+vim.keymap.set("n", "<leader>a", function()
+    harpoon:list():append()
+end)
+vim.keymap.set("n", "<C-e>", function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+vim.keymap.set("n", "<C-h>", function()
+    harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<C-t>", function()
+    harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<C-n>", function()
+    harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<C-s>", function()
+    harpoon:list():select(4)
+end)
+-- lazygit
+vim.keymap.set("n", "<leader>gg", function()
+    telescope.extensions.lazygit.lazygit()
+end)
+-- nvim-comment
+vim.keymap.set("n", "<leader>/", vim.cmd.CommentToggle)
+vim.keymap.set("v", "<leader>/", ":'<,'>CommentToggle<CR>vgv", { silent = true, noremap = true })
+-- zen-mode
+vim.keymap.set("n", "<leader>zm", function()
+    require("zen-mode").toggle()
+end)
+-- gitsigns
+vim.keymap.set("n", "<leader>tb", function()
+    require("gitsigns").toggle_current_line_blame()
+end)
+-- telescope
+vim.keymap.set("n", "<leader>pf", telescopeBuiltin.find_files)
+vim.keymap.set("n", "<leader>pp", function()
+    telescopeBuiltin.find_files({ hidden = true })
+end)
+vim.keymap.set("n", "<C-p>", telescopeBuiltin.git_files)
+-- vim.keymap.set("n",'<leader>ps', function() telescopeBuiltin.grep_string({search = vim.fn.input("Grep >")}) end)
+vim.keymap.set("n", "<leader>ps", telescopeBuiltin.live_grep)
+vim.keymap.set("n", "<leader>ph", telescopeBuiltin.help_tags)
+vim.keymap.set("n", "<leader>pe", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+vim.keymap.set("n", "<leader>pn", ":Telescope noice<CR>")
--- dotconfig/wezterm/fonts.lua
+++ dotconfig/wezterm/fonts.lua
@@ -2,29 +2,41 @@ local wezterm = require("wezterm")
 
 local F = {}
 local fonts = {
-	fira = {
-		font = {
-			family = "FiraCode Nerd Font",
-			harfbuzz_features = {
-				"clig=1",
-				"cv06=1",
-				"cv14=1",
-				"cv32=1",
-				"ss04=1",
-				"ss07=1",
-				"ss09=1",
-			},
-		},
-		size = 15,
-	},
+    fira = {
+        font = {
+            family = "FiraCode Nerd Font",
+            harfbuzz_features = {
+                "liga=0",
+                "clig=0",
+                "calt=0",
+                "zero", -- 0 with a dot, cv11-cv13
+                "cv01", -- a simple
+                "cv02", -- g simple
+                -- "cv03", -- i 03-06
+                -- "cv08", -- l changes 07-10
+                "ss01", -- r simple
+                -- "cv17", -- ~ further up
+                "ss05", -- @ sign shape
+                "ss04", -- $ sign shape
+                -- "cv18", -- % shape
+                "ss03", -- & shape
+                "cv16", -- * shape
+                -- "cv31", -- () shape
+                "cv29", -- {} shape
+                -- "cv30", -- | shape
+            },
+        },
+        size = 16,
+    },
 }
 F.setup = function(opts)
-	opts.font = wezterm.font_with_fallback({
-		fonts.fira.font,
-		"Noto Sans Emoji",
-	})
-	opts.font_size = fonts.fira.size
-	return opts
+    opts.font = wezterm.font_with_fallback({
+        fonts.fira.font,
+        "Noto Sans Emoji",
+    })
+    opts.font_size = fonts.fira.size
+    opts.harfbuzz_features = fonts.fira.font.harfbuzz_features
+    return opts
 end
 
 return F
