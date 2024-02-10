6b7bcbea4b2c03a90c3462f5ae76db5307953516
nerdwave <nick@nerdwave.io>
2024-02-10 16:15:27 +0100

    feat: indent lines nvim, multi bg setup wezterm

--- dotconfig/nvim/lua/nerdwave/plugins/catppuccin.lua
+++ dotconfig/nvim/lua/nerdwave/plugins/catppuccin.lua
@@ -1,52 +1,56 @@
 return {
-  "catppuccin/nvim",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = true, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      },
-      native_lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = { "italic" },
-          hints = { "italic" },
-          warnings = { "italic" },
-          information = { "italic" },
-        },
-        underlines = {
-          errors = { "underline" },
-          hints = { "underline" },
-          warnings = { "underline" },
-          information = { "underline" },
-        },
-        inlay_hints = {
-          background = true,
-        },
-      },
-      integrations = {
-        cmp = true,
-        treesitter = true,
-        notify = true,
-        harpoon = true,
-        mason = true,
-        telescope = {enabled = true,},
-        lsp_trouble = true,
-        which_key = true,
-        gitsigns = true,
-      },
-    })
-    vim.cmd 'colorscheme catppuccin'
-  end
+    "catppuccin/nvim",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            background = {     -- :h background
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = true, -- disables setting the background color.
+            show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+            term_colors = true,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+            dim_inactive = {
+                enabled = false,           -- dims the background color of inactive window
+                shade = "dark",
+                percentage = 0.15,         -- percentage of the shade to apply to the inactive window
+            },
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                },
+                underlines = {
+                    errors = { "underline" },
+                    hints = { "underline" },
+                    warnings = { "underline" },
+                    information = { "underline" },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
+            integrations = {
+                cmp = true,
+                treesitter = true,
+                notify = true,
+                harpoon = true,
+                mason = true,
+                telescope = { enabled = true, },
+                lsp_trouble = true,
+                which_key = true,
+                gitsigns = true,
+                indent_blankline = {
+                    enabled = true,
+                    scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+                    colored_indent_levels = false,
+                },
+            },
+        })
+        vim.cmd 'colorscheme catppuccin'
+    end
 }
-
--- dotconfig/wezterm/background.lua
+++ dotconfig/wezterm/background.lua
@@ -1,27 +1,56 @@
 local wezterm = require("wezterm")
 
 local function setup(opts)
-	local hostname = wezterm.hostname()
-	local wallpaper_prefix_map = {
-		["debian"] = os.getenv("HOME") .. "/Pictures/",
-		["HBV83038406"] = os.getenv("HOME") .. "/Documents/",
-	}
-	local wallpaper_map = {
-		["debian"] = "alex-knight-5-GNa303REg-unsplash.jpg",
-		["HBV83038406"] = "wp.jpg",
-	}
-	if wallpaper_prefix_map[hostname] ~= nil then
-		opts.window_background_image = wallpaper_prefix_map[hostname] .. wallpaper_map[hostname]
-		opts.window_background_image_hsb = {
-			brightness = 0.025,
-			hue = 1.0,
-			saturation = 1.0,
-		}
-	end
-	opts.window_background_opacity = 1.00
-	return opts
+    local hostname = wezterm.hostname()
+    local host_wallpaper_map = {
+        ["debian"] = {
+            prefix = os.getenv("HOME") .. "/Pictures/",
+            wallpapers = {
+                {
+                    image = "backiee-257465-landscape.jpg",
+                    hsb = {
+                        brightness = 0.024,
+                        hue = 1.0,
+                        saturation = 1.0,
+                    }
+                },
+                {
+                    image = "alex-knight-5-GNa303REg-unsplash.jpg",
+                    hsb = {
+                        brightness = 0.030,
+                        hue = 1.0,
+                        saturation = 1.0,
+                    }
+                }
+            },
+
+        },
+        ["HBV83038406"] = {
+            prefix = os.getenv("HOME") .. "/Documents/",
+            wallpapers = {
+                {
+                    image = "wp.jpg",
+                    hsb = {
+                        brightness = 0.025,
+                        hue = 1.0,
+                        saturation = 1.0,
+                    }
+                }
+            }
+
+        }
+    }
+    if host_wallpaper_map[hostname] ~= nil then
+        local wallpaper_map = host_wallpaper_map[hostname]
+        local prefix = wallpaper_map.prefix
+        local wallpaper = wallpaper_map.wallpapers[math.random(#wallpaper_map.wallpapers)]
+        opts.window_background_image = prefix .. wallpaper.image
+        opts.window_background_image_hsb = wallpaper.hsb
+    end
+    opts.window_background_opacity = 1.00
+    return opts
 end
 
 return {
-	setup = setup,
+    setup = setup,
 }
