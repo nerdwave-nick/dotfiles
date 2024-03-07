f9f1fc04a511064c213fd4be024c844a950ec859
Nick Mueller <nick.mueller@bauerxcel.de>
2024-03-07 14:52:29 +0100

    feat: commenting for jsx and some keybindings n stuff

--- dotconfig/nvim/lua/nerdwave/plugins/gitsign.lua
+++ dotconfig/nvim/lua/nerdwave/plugins/gitsign.lua
@@ -3,6 +3,5 @@ return {
   config = function()
     local gitsigns = require('gitsigns')
     gitsigns.setup()
-    gitsigns.toggle_current_line_blame()
   end,
 }
--- dotconfig/nvim/lua/nerdwave/plugins/nvim-comment.lua
+++ dotconfig/nvim/lua/nerdwave/plugins/nvim-comment.lua
@@ -1,4 +1,16 @@
 return {
   'terrortylor/nvim-comment',
-  config = function() require('nvim_comment').setup({ create_mappings = false, comment_empty = false }) end,
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
+  config = function()
+    require('ts_context_commentstring').setup({
+      enable_autocmd = false,
+    })
+    require('nvim_comment').setup({
+      create_mappings = false,
+      comment_empty = false,
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    })
+  end,
 }
