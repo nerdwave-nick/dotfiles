f6fd12cccd3b91206744fae8e29de2feaefc4995
nerdwave <nick@nerdwave.io>
2024-04-04 12:41:17 +0200

    feat: cmp remapping and disabled standard mapping because hell yeah

--- dotconfig/nvim/lua/nerdwave/plugins/nvim-cmp.lua
+++ dotconfig/nvim/lua/nerdwave/plugins/nvim-cmp.lua
@@ -60,15 +60,17 @@ local cmp_setup = function()
       max_view_entries = 120,
     },
     -- You can set mappings if you want
-    mapping = cmp.mapping.preset.insert({
+    mapping = {
       ['<C-CR>'] = cmp.mapping.confirm({
         select = false,
         behavior = cmp.ConfirmBehavior.Replace,
       }),
+      ['<C-t>'] = cmp.mapping.select_next_item({ }),
+      ['<C-n>'] = cmp.mapping.select_prev_item({ }),
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-w>'] = cmp.mapping.close(),
-    }),
+    },
     snippet = {
       expand = function(args) require('luasnip').lsp_expand(args.body) end,
     },
