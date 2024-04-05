d33ccce6472d8a68f1b27b922401d05c5b8a4b56
nerdwave <nick@nerdwave.io>
2024-04-05 02:12:48 +0200

    feat: iferr

--- dotconfig/nvim/lua/nerdwave/keybinds.lua
+++ dotconfig/nvim/lua/nerdwave/keybinds.lua
@@ -123,7 +123,15 @@ vim.keymap.set(
   { noremap = true, desc = 'Open file browser at current path' }
 )
 vim.keymap.set('n', '<leader>pn', ':Telescope noice<CR>', { noremap = true, desc = 'Open noice in telescope' })
-vim.keymap.set('n', '<leader>pt', ':TodoTelescope<CR>', { noremap = true, silent=true, desc = 'Open todos in telescope' })
+vim.keymap.set(
+  'n',
+  '<leader>pt',
+  ':TodoTelescope<CR>',
+  { noremap = true, silent = true, desc = 'Open todos in telescope' }
+)
 
 -- lazygirl
 vim.keymap.set('n', '<leader>gg', function() telescope.extensions.lazygit.lazygit() end)
+
+-- go
+vim.keymap.set('n', '<leader>ee', 'oif err != nil {<CR><Tab>return err<CR>}<ESC>')
