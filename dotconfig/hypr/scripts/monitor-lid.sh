1728e814c7163526172d151de687d7ac1c2e2072
nerdwave-nick <nick@nerdwave.io>
2024-07-30 21:00:26 +0200

    feat: more script fuckery

--- dotconfig/hypr/scripts/monitor-lid.sh
+++ dotconfig/hypr/scripts/monitor-lid.sh
@@ -2,10 +2,13 @@
 
 monitors="$(hyprctl monitors)"
 if [[ "$monitors" =~ "\sDP-[0-9]+" || "$monitors" =~ "ID 1" ]]; then
-  if [[ $1 == "open" ]]; then
-    hyprctl keyword monitor "eDP-1,1920x1080,2560x0,1"
+  state="$(cat /proc/acpi/button/lid/LID/state | sed -n -e 's/^.*state:\s*//p')"
+  if [[ "$state" == "open" ]]; then
+    echo ":: enabling internal monitor due to laptop lid state: $state"
+    hyprctl keyword monitor "eDP-1,1920x1080,2560x0,1" > /dev/null
   else
-    hyprctl keyword monitor "eDP-1,disable"
+    echo ":: disabling internal monitor due to laptop lid state: $state"
+    hyprctl keyword monitor "eDP-1,disable" > /dev/null
   fi
 else 
   echo "$monitors didn't work"
