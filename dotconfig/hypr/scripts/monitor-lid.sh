e21d1be6826f1bda2e3a27b4062165c0b09f2139
nerdwave-nick <nick@nerdwave.io>
2024-08-01 00:08:48 +0200

    feat: clamshel shit fixed I guess, including a functioning waybar c:

--- dotconfig/hypr/scripts/monitor-lid.sh
+++ dotconfig/hypr/scripts/monitor-lid.sh
@@ -4,10 +4,10 @@ monitors="$(hyprctl monitors)"
 if [[ "$monitors" =~ "\sDP-[0-9]+" || "$monitors" =~ "ID 1" ]]; then
   state="$(cat /proc/acpi/button/lid/LID/state | sed -n -e 's/^.*state:\s*//p')"
   if [[ "$state" == "open" ]]; then
-    notify-send "enabling internal monitor due to laptop lid state: $state"
+    notify-send "Laptop opened, enabling internal monitor"
     hyprctl keyword monitor "eDP-1,1920x1080,2560x0,1" > /dev/null
   else
-    notify-send "disabling internal monitor due to laptop lid state: $state"
+    notify-send "Laptop closed, disabling internal monitor"
     hyprctl keyword monitor "eDP-1,disable" > /dev/null
   fi
 else 
